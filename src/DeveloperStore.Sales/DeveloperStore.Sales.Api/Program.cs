ade8f485f5618a3fb2609f81685a91149411f544
Matheus Maximiano <matheus.nicks@gmail.com>
2025-01-18 20:38:06 -0300

    feat: setup database connection

--- src/DeveloperStore.Sales/DeveloperStore.Sales.Api/Program.cs
+++ src/DeveloperStore.Sales/DeveloperStore.Sales.Api/Program.cs
@@ -1,8 +1,13 @@
+using DeveloperStore.Sales.Infra.IoC.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbConnection(builder.Configuration);
+builder.Services.AddDependencyInjection(builder.Configuration);
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
---
+++ src/DeveloperStore.Sales/DeveloperStore.Sales.Infra.IoC/Configuration/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using DeveloperStore.Sales.Infra.Data.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DeveloperStore.Sales.Infra.IoC.Configuration;
+
+public static class DependencyInjection
+{
+    public static void AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<SalesContext>();
+    }
+}
