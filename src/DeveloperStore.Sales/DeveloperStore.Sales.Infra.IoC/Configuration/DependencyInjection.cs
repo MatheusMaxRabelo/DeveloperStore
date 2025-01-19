f83098f489a649a420394f440f0a4e85c22d19d3
Matheus Maximiano <matheus.nicks@gmail.com>
2025-01-18 21:15:12 -0300

    feat: setup RabbitMQ connection

--- src/DeveloperStore.Sales/DeveloperStore.Sales.Infra.IoC/Configuration/DependencyInjection.cs
+++ src/DeveloperStore.Sales/DeveloperStore.Sales.Infra.IoC/Configuration/DependencyInjection.cs
@@ -1,6 +1,8 @@
-﻿using DeveloperStore.Sales.Infra.Data.Context;
+﻿using DeveloperStore.Sales.Domain.RabbitMQ;
+using DeveloperStore.Sales.Infra.Data.Context;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
 
 namespace DeveloperStore.Sales.Infra.IoC.Configuration;
 
@@ -9,5 +11,30 @@ public static class DependencyInjection
     public static void AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<SalesContext>();
+
+        RabbitMQConfig(services, configuration);
+    }
+
+    private static void RabbitMQConfig(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IRabbitMQConnection>(sp =>
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = configuration["Bus:HostName"]
+            };
+
+            if (!string.IsNullOrEmpty(configuration["Bus:UserName"]))
+            {
+                factory.UserName = configuration["Bus:UserName"];
+            }
+
+            if (!string.IsNullOrEmpty(configuration["Bus:Password"]))
+            {
+                factory.Password = configuration["Bus:Password"];
+            }
+
+            return new RabbitMQConnection(factory);
+        });
     }
 }
