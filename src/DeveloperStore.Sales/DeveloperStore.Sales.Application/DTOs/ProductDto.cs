ee8b2d4b97bb7ba3f0b3b1ae62e9824a7b11d948
Matheus Maximiano <matheus.nicks@gmail.com>
2025-01-20 14:10:12 -0300

    feat: implement UpdateSale Command to update sale data

--- src/DeveloperStore.Sales/DeveloperStore.Sales.Application/DTOs/ProductDto.cs
+++ src/DeveloperStore.Sales/DeveloperStore.Sales.Application/DTOs/ProductDto.cs
@@ -1,13 +1,30 @@
-﻿namespace DeveloperStore.Sales.Application.DTOs;
+﻿using System.Text.Json.Serialization;
+
+namespace DeveloperStore.Sales.Application.DTOs;
 
 public class ProductDto
 {
+    [JsonPropertyName("id")]
     public int Id { get; set; }
+
+    [JsonPropertyName("title")]
     public string Title { get; set; } = string.Empty;
+
+    [JsonPropertyName("unitPrice")]
     public decimal UnitPrice { get; set; } = 0m;
+
+    [JsonPropertyName("image")]
     public string Image { get; set; }
+
+    [JsonPropertyName("quantity")]
     public int Quantity { get; set; }
+
+    [JsonPropertyName("discount")]
     public decimal Discount { get; set; }
+
+    [JsonPropertyName("totalAmount")]
     public decimal TotalAmount { get; set; }
+
+    [JsonPropertyName("isCancelled")]
     public bool IsCancelled { get; set; } = false;
 }
