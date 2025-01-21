b3ceb528cb7b38ecc2b567f95f4b964834d676e3
Matheus Maximiano <matheus.nicks@gmail.com>
2025-01-21 14:03:27 -0300

    fix: resolve issues in sale update and query functionalities

--- src/DeveloperStore.Sales/DeveloperStore.Sales.Domain/Interfaces/Services/ISalesService.cs
+++ src/DeveloperStore.Sales/DeveloperStore.Sales.Domain/Interfaces/Services/ISalesService.cs
@@ -5,7 +5,7 @@ namespace DeveloperStore.Sales.Domain.Interfaces.Services;
 
 public interface ISalesService
 {
-    Task<List<SalesModel>> GetSalesAsync(Dictionary<string, string> filter);
+    Task<(List<SalesModel> sales,int totalItems)> GetSalesAsync(Dictionary<string, string> filter);
     Task<SalesModel> GetSaleByIdAsync(int id);
     Task<SalesModel> CreateSaleAsync(Sale sale);
     Task<SalesModel> UpdateSaleAsync(int id, Sale sale);
